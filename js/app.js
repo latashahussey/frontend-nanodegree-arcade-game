3529b91d7633c201951fc1791c0afea072b9db18
sugarcookie <tasha@lhussey.com>
2017-03-13 13:59:16 -0500

    Get game board on screen

--- js/app.js
+++ js/app.js
@@ -1,16 +1,13 @@
-
-
-
 // Enemies our player must avoid
-var Enemy = function(locX, locY) {
+var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.locX = locX;
-    this.locY = locY;
+    //this.locX = locX;
+    //this.locY = locY;
 };
 
 // Update the enemy's position, required method for game
@@ -19,7 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.speed += 75 * dt; // Move enemy 75  pixels per second
+  //  this.speed += 75 * dt; // Move enemy 75  pixels per second
 
 };
 
